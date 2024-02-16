7aa4140febb671d7bd64a80d3bc49a18a14e405c
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2024-02-16 17:36:33 -0500

    Initial commit.

---
+++ iconquer.js
@@ -0,0 +1,689 @@
+let countries = {
+	"Alaska": {
+		"dotOffsetX": 0.0,
+		"dotOffsetY": 13,
+		"neighbors": [
+			"Northwest Territories",
+			"Alberta",
+			"Kamchatka"
+		],
+		"x": 8,
+		"y": 696,
+		"width": 183,
+		"height": 137,
+    "left": "Kamchatka", 
+    "right": "Northwest Territories", 
+	},
+	"Alberta": {
+		"dotOffsetX": 19,
+		"dotOffsetY": 0.0,
+		"neighbors": [
+			"Alaska",
+			"Northwest Territories",
+			"Ontario",
+			"Western United States"
+		],
+		"x": 165,
+		"y": 662,
+		"width": 165,
+		"height": 83
+	},
+	"Argentina": {
+		"dotOffsetX": 0.0,
+		"dotOffsetY": 27,
+		"neighbors": [
+			"Peru",
+			"Brazil"
+		],
+		"x": 471,
+		"y": 8,
+		"width": 142,
+		"height": 265
+	},
+  "Brazil": {
+  	"dotOffsetX": 13,
+  	"dotOffsetY": 27,
+  	"neighbors": [
+  		"Venezuela",
+  		"Peru",
+  		"Argentina",
+  		"North Africa"
+  	],
+  	"x": 495,
+  	"y": 178,
+  	"width": 214,
+  	"height": 229,
+    "up": "Venezuela"
+  },
+	"Central America": {
+		"dotOffsetX": -24,
+		"dotOffsetY": 5,
+		"height": 83,
+		"neighbors": [
+			"Eastern United States",
+			"Western United States",
+			"Venezuela"
+		],
+		"width": 147,
+		"x": 268,
+		"y": 413,
+		"width": 266,
+		"height": 151
+	},
+	"China": {
+		"dotOffsetX": 6,
+		"dotOffsetY": 5,
+		"neighbors": [
+			"Mongolia",
+			"Siberia",
+			"Ural",
+			"Kazakstan",
+			"India",
+			"Siam"
+		],
+		"x": 1198,
+		"y": 438,
+		"width": 286,
+		"height": 202
+	},
+	"Congo": {
+		"dotOffsetX": 5,
+		"dotOffsetY": 6,
+		"neighbors": [
+			"North Africa",
+			"East Africa",
+			"South Africa"
+		],
+		"x": 862,
+		"y": 260,
+		"width": 113,
+		"height": 110
+	},
+	"East Africa": {
+		"dotOffsetX": 1,
+		"dotOffsetY": -2,
+		"neighbors": [
+			"Egypt",
+			"North Africa",
+			"Congo",
+			"South Africa",
+			"Madagascar",
+			"Middle East"
+		],
+		"x": 931,
+		"y": 270,
+		"width": 158,
+		"height": 202
+	},
+	"Eastern Australia": {
+		"dotOffsetX": 9,
+		"dotOffsetY": -4,
+		"neighbors": [
+			"Western Australia",
+			"New Guinea"
+		],
+		"x": 1502,
+		"y": 61,
+		"width": 140,
+		"height": 234
+	},
+	"Eastern United States": {
+		"dotOffsetX": -5,
+		"dotOffsetY": -2,
+		"neighbors": [
+			"Central America",
+			"Western United States",
+			"Ontario",
+			"Quebec"
+		],
+		"x": 331,
+		"y": 518,
+		"width": 209,
+		"height": 147
+	},
+	"Egypt": {
+		"dotOffsetX": -3,
+		"dotOffsetY": 4,
+		"neighbors": [
+			"Southern Europe",
+			"Middle East",
+			"East Africa",
+			"North Africa"
+		],
+		"x": 857,
+		"y": 456,
+		"width": 149,
+		"height": 83
+	},
+	"Greenland": {
+		"dotOffsetX": 108,
+		"dotOffsetY": 8,
+		"neighbors": [
+			"Northwest Territories",
+			"Ontario",
+			"Quebec",
+			"Iceland"
+		],
+		"x": 272,
+		"y": 731,
+		"width": 559,
+		"height": 208
+	},
+	"Iceland": {
+		"dotOffsetX": 0.0,
+		"dotOffsetY": -9,
+		"neighbors": [
+			"Greenland",
+			"Scandinavia",
+			"United Kingdom"
+		],
+		"x": 717,
+		"y": 711,
+		"width": 60,
+		"height": 38
+	},
+	"India": {
+		"dotOffsetX": 1,
+		"dotOffsetY": 8,
+		"neighbors": [
+			"Middle East",
+			"Kazakstan",
+			"China",
+			"Siam"
+		],
+		"x": 1126,
+		"y": 367,
+		"width": 201,
+		"height": 195
+	},
+	"Indonesia": {
+		"dotOffsetX": 13,
+		"dotOffsetY": -22,
+		"neighbors": [
+			"Siam",
+			"Western Australia",
+			"New Guinea"
+		],
+		"x": 1310,
+		"y": 270,
+		"width": 184,
+		"height": 189
+	},
+	"Irkutsk": {
+		"dotOffsetX": -6,
+		"dotOffsetY": 6,
+		"neighbors": [
+			"Siberia",
+			"Yakutia",
+			"Kamchatka",
+			"Mongolia"
+		],
+		"x": 1329,
+		"y": 621,
+		"width": 161,
+		"height": 99
+	},
+	"Japan": {
+		"dotOffsetX": 7,
+		"dotOffsetY": -13,
+		"neighbors": [
+			"Mongolia",
+			"Kamchatka"
+		],
+		"x": 1476,
+		"y": 511,
+		"width": 106,
+		"height": 121
+	},
+	"Kamchatka": {
+		"dotOffsetX": 51,
+		"dotOffsetY": 57,
+		"neighbors": [
+			"Yakutia",
+			"Irkutsk",
+			"Mongolia",
+			"Japan",
+			"Alaska"
+		],
+		"x": 1470,
+		"y": 582,
+		"width": 339,
+		"height": 203,
+    "left": "Irkutsk",
+    "right": "Alaska"
+	},
+	"Kazakstan": {
+		"dotOffsetX": -3,
+		"dotOffsetY": 5,
+		"neighbors": [
+			"Russia",
+			"Ural",
+			"China",
+			"India",
+			"Middle East"
+		],
+		"x": 1069,
+		"y": 513,
+		"width": 193,
+		"height": 156
+	},
+	"Madagascar": {
+		"dotOffsetX": 9,
+		"dotOffsetY": -5,
+		"neighbors": [
+			"East Africa",
+			"South Africa"
+		],
+		"x": 1034,
+		"y": 180,
+		"width": 51,
+		"height": 95
+	},
+	"Middle East": {
+		"dotOffsetX": 0.0,
+		"dotOffsetY": 0.0,
+		"neighbors": [
+			"Russia",
+			"Kazakstan",
+			"India",
+			"East Africa",
+			"Egypt",
+			"Southern Europe"
+		],
+		"x": 943,
+		"y": 408,
+		"width": 200,
+		"height": 200,
+    "left": "Egypt"
+	},
+	"Mongolia": {
+		"dotOffsetX": 20,
+		"dotOffsetY": 12,
+		"neighbors": [
+			"Siberia",
+			"Irkutsk",
+			"Kamchatka",
+			"Japan",
+			"China"
+		],
+		"x": 1297,
+		"y": 527,
+		"width": 224,
+		"height": 136
+	},
+	"New Guinea": {
+		"dotOffsetX": -1,
+		"dotOffsetY": 11,
+		"neighbors": [
+			"Indonesia",
+			"Eastern Australia",
+			"Western Australia"
+		],
+		"x": 1477,
+		"y": 264,
+		"width": 194,
+		"height": 89
+	},
+	"North Africa": {
+		"dotOffsetX": -10,
+		"dotOffsetY": 4,
+		"neighbors": [
+			"Western Europe",
+			"Southern Europe",
+			"Egypt",
+			"East Africa",
+			"Congo",
+			"Brazil"
+		],
+		"x": 711,
+		"y": 314,
+		"width": 252,
+		"height": 250,
+    "down": "Congo", 
+    "right": "Egypt", 
+    "up": "Western Europe", 
+	},
+	"Northern Europe": {
+		"dotOffsetX": -7,
+		"dotOffsetY": -10,
+		"neighbors": [
+			"United Kingdom",
+			"Scandinavia",
+			"Russia",
+			"Southern Europe",
+			"Western Europe"
+		],
+		"x": 827,
+		"y": 620,
+		"width": 131,
+		"height": 70,
+    "down": "Southern Europe"
+	},
+	"Northwest Territories": {
+		"dotOffsetX": 3,
+		"dotOffsetY": -41,
+		"neighbors": [
+			"Alaska",
+			"Alberta",
+			"Ontario",
+			"Greenland"
+		],
+		"x": 156,
+		"y": 744,
+		"width": 203,
+		"height": 150,
+    "down": "Alberta"
+	},
+	"Ontario": {
+		"dotOffsetX": -18,
+		"dotOffsetY": 17,
+		"neighbors": [
+			"Northwest Territories",
+			"Greenland",
+			"Quebec",
+			"Eastern United States",
+			"Western United States",
+			"Alberta"
+		],
+		"x": 329,
+		"y": 618,
+		"width": 167,
+		"height": 128,
+    "right": "Quebec",
+    "left": "Alberta"
+	},
+	"Peru": {
+		"dotOffsetX": -20,
+		"dotOffsetY": 0.0,
+		"neighbors": [
+			"Venezuela",
+			"Brazil",
+			"Argentina"
+		],
+		"x": 448,
+		"y": 211,
+		"width": 150,
+		"height": 177
+	},
+	"Quebec": {
+		"dotOffsetX": -20,
+		"dotOffsetY": 4,
+		"neighbors": [
+			"Eastern United States",
+			"Ontario",
+			"Greenland"
+		],
+		"x": 468,
+		"y": 626,
+		"width": 150,
+		"height": 138,
+    "right": "Ontario"
+	},
+	"Russia": {
+		"dotOffsetX": -40,
+		"dotOffsetY": 2,
+		"neighbors": [
+			"Scandinavia",
+			"Northern Europe",
+			"Southern Europe",
+			"Middle East",
+			"Kazakstan",
+			"Ural"
+		],
+		"x": 933,
+		"y": 558,
+		"width": 222,
+		"height": 228,
+    "right": "Ural"
+	},
+	"Scandinavia": {
+		"dotOffsetX": -19,
+		"dotOffsetY": -6,
+		"neighbors": [
+			"Iceland",
+			"United Kingdom",
+			"Northern Europe",
+			"Russia"
+		],
+		"x": 829,
+		"y": 665,
+		"width": 151,
+		"height": 130
+	},
+	"Siam": {
+		"dotOffsetX": 2,
+		"dotOffsetY": 17,
+		"neighbors": [
+			"India",
+			"China",
+			"Indonesia"
+		],
+		"x": 1293,
+		"y": 343,
+		"width": 104,
+		"height": 168
+	},
+	"Siberia": {
+		"dotOffsetX": 6,
+		"dotOffsetY": -5,
+		"neighbors": [
+			"Ural",
+			"China",
+			"Mongolia",
+			"Irkutsk",
+			"Yakutia"
+		],
+		"x": 1190,
+		"y": 628,
+		"width": 217,
+		"height": 220
+	},
+	"South Africa": {
+		"dotOffsetX": -13,
+		"dotOffsetY": -10,
+		"neighbors": [
+			"Congo",
+			"East Africa",
+			"Madagascar"
+		],
+		"x": 864,
+		"y": 121,
+		"width": 171,
+		"height": 188
+	},
+	"Southern Europe": {
+		"dotOffsetX": 14,
+		"dotOffsetY": 14,
+		"neighbors": [
+			"Western Europe",
+			"Northern Europe",
+			"Russia",
+			"Middle East",
+			"Egypt",
+			"North Africa"
+		],
+		"x": 843,
+		"y": 545,
+		"width": 130,
+		"height": 88,
+    "down": "Egypt",
+    "up": "Northern Europe", 
+	},
+	"United Kingdom": {
+		"dotOffsetX": 0.0,
+		"dotOffsetY": -4,
+		"neighbors": [
+			"Iceland",
+			"Scandinavia",
+			"Northern Europe",
+			"Western Europe"
+		],
+		"x": 751,
+		"y": 629,
+		"width": 183,
+		"height": 137,
+		"width": 76,
+		"height": 72
+	},
+	"Ural": {
+		"dotOffsetX": -9,
+		"dotOffsetY": 2,
+		"neighbors": [
+			"Russia",
+			"Kazakstan",
+			"China",
+			"Siberia"
+		],
+		"x": 1034,
+		"y": 614,
+		"width": 243,
+		"height": 196,
+    "down": "Kazakstan",
+    "right": "Siberia"
+	},
+	"Venezuela": {
+		"dotOffsetX": -11,
+		"dotOffsetY": 4,
+		"neighbors": [
+			"Central America",
+			"Peru",
+			"Brazil"
+		],
+		"x": 458,
+		"y": 372,
+		"width": 164,
+		"height": 84
+	},
+	"Western Australia": {
+		"dotOffsetX": 4,
+		"dotOffsetY": 0.0,
+		"neighbors": [
+			"Indonesia",
+			"New Guinea",
+			"Eastern Australia"
+		],
+		"x": 1409,
+		"y": 119,
+		"width": 139,
+		"height": 163
+	},
+	"Western Europe": {
+		"dotOffsetX": -4,
+		"dotOffsetY": 0.0,
+		"neighbors": [
+			"United Kingdom",
+			"Northern Europe",
+			"Southern Europe",
+			"North Africa"
+		],
+		"x": 755,
+		"y": 548,
+		"width": 111,
+		"height": 108
+	},
+	"Western United States": {
+		"dotOffsetX": 2,
+		"dotOffsetY": 3,
+		"neighbors": [
+			"Alberta",
+			"Ontario",
+			"Eastern United States",
+			"Central America"
+		],
+		"x": 224,
+		"y": 557,
+		"width": 167,
+		"height": 106
+	},
+	"Yakutia": {
+		"dotOffsetX": -36,
+		"dotOffsetY": -6,
+		"neighbors": [
+			"Siberia",
+			"Irkutsk",
+			"Kamchatka"
+		],
+		"x": 1371,
+		"y": 687,
+		"width": 279,
+		"height": 133
+	}
+};
+
+const main = document.getElementById("main");
+
+Object.keys(countries).forEach((name) => {
+  let country = countries[name];
+  let img = document.createElement("img");
+  img.classList.add("country");
+  img.src = `images/${name}.png`;
+  img.style.width = country.width + 'px';
+  img.style.height = country.height + 'px';
+  img.style.bottom = country.y + 'px';
+  img.style.left = country.x + 'px';
+  main.appendChild(img);
+  country.name = name;
+  country.img = img;
+  country.dotX = country.x + country.width / 2.0 + country.dotOffsetX;
+  country.dotY = country.y + country.height / 2.0 + country.dotOffsetY;
+  country.neighborCountries = country.neighbors.map((neighbor) => countries[neighbor]);
+});
+
+document.addEventListener("keydown", function(event) {
+	switch (event.key) {
+    case "ArrowUp": up(); break;
+    case "ArrowDown": down(); break;
+    case "ArrowLeft": left(); break;
+    case "ArrowRight": right(); break;      
+		default: return;
+	}
+	event.preventDefault();
+});
+
+let selected = countries["North Africa"];
+
+function select() {
+  selected.img.classList.add("selected");
+  console.log(selected.name);
+}
+select();
+
+function deselect() {
+  selected.img.classList.remove("selected");
+}
+
+function up() {
+  deselect();
+  selected = findNeighbor(selected, (c,n) => n.dotY, "up");
+  select();
+}
+
+function down() {
+  deselect();
+  selected = findNeighbor(selected, (c,n) => - n.dotY, "down");
+  select();
+}
+
+function left() {
+  deselect();
+  selected = findNeighbor(selected, (c,n) => - n.dotX, "left");
+  select();
+}
+
+function right() {
+  deselect();
+  selected = findNeighbor(selected, (c,n) => n.dotX, "right");
+  select();
+}
+
+// Calls the test function on all the country's neighbors
+// and returns the one with the largest result.
+// If the country has a property called direction, 
+// returns the country with that name instead.
+function findNeighbor(country, test, direction) {
+  if (country[direction]) return countries[country[direction]];
+  return country.neighborCountries.reduce((best, neighbor) => {
+    if (best == null) return neighbor;
+    return test(country, neighbor) > test(country, best) ? neighbor : best;
+  });
+} 
